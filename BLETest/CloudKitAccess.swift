68df42f9ff176cd98f0bd5714ce38efa0d65e968
Joachim Kittelberger <jkittelberger@kibesoft.de>
2023-04-25 16:43:41 +0200

    Fixed Merge

---
+++ BLETest/CloudKitAccess.swift
@@ -0,0 +1,17 @@
+//
+//  CloudKitAccess.swift
+//  BLETest
+//
+//  Created by Joachim Kittelberger on 07.09.18.
+//  Copyright © 2018 Joachim Kittelberger. All rights reserved.
+//
+
+import Foundation
+
+
+class CloudKitAccess {
+    let container = CKContainer.defaultContainer()
+    
+    
+    
+}
